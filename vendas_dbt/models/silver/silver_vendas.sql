51a0b9db472b73b5aad66fa83a242c5233f86b46
Luiz Fernando <luizfsoliveira.lm@gmail.com>
2024-12-13 12:02:24 -0300

    second commit

---
+++ vendas_dbt/models/silver/silver_vendas.sql
@@ -0,0 +1,20 @@
+{{ config(materialized='view') }}
+
+WITH cleaned_data AS (
+    SELECT 
+        vendedor,
+        email, 
+        DATE(data) AS data,
+        valor, 
+        quantidade, 
+        produto
+    FROM 
+        {{ ref('bronze_vendas') }}
+        
+    WHERE 
+        valor > 0 
+        AND valor < 8000
+        AND data <= CURRENT_DATE
+)
+
+SELECT * FROM cleaned_data
